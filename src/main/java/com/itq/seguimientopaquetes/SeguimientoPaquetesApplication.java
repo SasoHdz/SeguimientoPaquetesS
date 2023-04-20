5759d50569901b7c1f83a79f32d207c4d9a4af0a
SasoHdz <isaacrdzcmd@gmail.com>
2023-04-19 21:05:57 -0600

    First commit

---
+++ src/main/java/com/itq/seguimientopaquetes/SeguimientoPaquetesApplication.java
@@ -0,0 +1,13 @@
+package com.itq.seguimientopaquetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeguimientoPaquetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeguimientoPaquetesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itq/seguimientopaquetes/SeguimientoPaquetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itq.seguimientopaquetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguimientoPaquetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
