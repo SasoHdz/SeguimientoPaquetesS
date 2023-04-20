7da6eac79e16ae2507c2e209fd55626f12b8ee4c
SasoHdz <isaacrdzcmd@gmail.com>
2023-04-19 23:51:31 -0600

    Operacion crear paquete

---
+++ src/main/java/com/itq/seguimientopaquetes/business/PaqueteService.java
@@ -0,0 +1,15 @@
+package com.itq.seguimientopaquetes.business;
+
+import com.itq.seguimientopaquetes.dto.Ack;
+import com.itq.seguimientopaquetes.dto.Paquete;
+
+public class PaqueteService {
+    public Ack insertarPaquete(Paquete pac) {
+		Ack ack = new Ack();
+		ack.setDescription("dsdasd");
+		
+		ack.setCode(0);
+		
+		return ack;
+	}
+}
---
+++ src/main/java/com/itq/seguimientopaquetes/service/PaqueteEndpoint.java
@@ -0,0 +1,26 @@
+package com.itq.seguimientopaquetes.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.itq.seguimientopaquetes.business.PaqueteService;
+import com.itq.seguimientopaquetes.dto.Ack;
+import com.itq.seguimientopaquetes.dto.Paquete;
+
+@Endpoint
+public class PaqueteEndpoint {
+
+	@Autowired PaqueteService paqueteService;
+
+    private static final String NAMESPACE_URI = "http://com.rastreo";
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "paquete")
+	@ResponsePayload
+
+    public Ack paqueteRegistration(@RequestPayload Paquete request){
+        Ack response = paqueteService.insertarPaquete(request);
+        return response;
+    }
+}
