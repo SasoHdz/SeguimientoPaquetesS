86ff0d6e252e53b994d8236ffd051fc8e8997347
GMrlsN <111917972+GMrlsN@users.noreply.github.com>
2023-04-21 19:35:27 -0600

    entidades para adaptar objetos a las tablas de la base de datos

--- src/main/java/com/itq/seguimientopaquetes/business/PaqueteService.java
+++ src/main/java/com/itq/seguimientopaquetes/business/PaqueteService.java
@@ -4,10 +4,15 @@ import org.springframework.stereotype.Component;
 
 import com.itq.seguimientopaquetes.dto.Ack;
 import com.itq.seguimientopaquetes.dto.Paquete;
+import com.itq.seguimientopaquetes.entity.paqueteRepository;
+import com.itq.seguimientopaquetes.entity.registroPaqueteRepository;
+import com.itq.seguimientopaquetes.entity.ubicacionRepository;
+import com.itq.seguimientopaquetes.entity.usuarioRepository;
 
 @Component
 public class PaqueteService { 
     public Ack insertarPaquete(Paquete paquete) {
+		
 		Ack ack = new Ack();
 		System.out.println("Paquete Recibido: '" + paquete.getDescripcion() + "'");
 
--- src/main/java/com/itq/seguimientopaquetes/business/SeguimientoService.java
+++ src/main/java/com/itq/seguimientopaquetes/business/SeguimientoService.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Component;
 import com.itq.seguimientopaquetes.dto.Ack;
 import com.itq.seguimientopaquetes.dto.EstadoType;
 import com.itq.seguimientopaquetes.dto.IdPaquete;
-
+import com.itq.seguimientopaquetes.entity.paqueteRepository;
+import com.itq.seguimientopaquetes.entity.registroPaqueteRepository;
+import com.itq.seguimientopaquetes.entity.ubicacionRepository;
+import com.itq.seguimientopaquetes.entity.usuarioRepository;
 @Component
 public class SeguimientoService {
     public Ack seguimientoOperation(IdPaquete idPaquete) {
