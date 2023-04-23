9cee60369f2ae1f85c4324d2d96bce6c98815b20
SasoHdz <isaacrdzcmd@gmail.com>
2023-04-23 11:17:01 -0600

    Arreglando error 404

--- src/main/java/com/itq/seguimientopaquetes/entity/usuarioRepository.java
+++ src/main/java/com/itq/seguimientopaquetes/entity/usuarioRepository.java
@@ -2,13 +2,10 @@ package com.itq.seguimientopaquetes.entity;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.stereotype.Repository;
 
 //import com.itq.seguimientopaquetes.dto.Cliente;
 //import com.itq.seguimientopaquetes.entity.Usuario;
 
-@Repository
 public interface usuarioRepository extends JpaRepository<Usuario, Integer> {
     // Aquí se pueden agregar métodos personalizados para usuarioRepository, si es necesario
 }
